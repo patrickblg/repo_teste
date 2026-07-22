d6265c62dd319b3e000cdeeea97f5fd1fea52618
patrickblg <blaga.patrick@yahoo.com>
2026-07-22 20:32:39 +0300

    initial commit

---
+++ testmsg1/src/main/java/com/example/testmsg1/Testmsg1Application.java
@@ -0,0 +1,13 @@
+package com.example.testmsg1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testmsg1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testmsg1Application.class, args);
+	}
+
+}
---
+++ testmsg1/src/test/java/com/example/testmsg1/Testmsg1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testmsg1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testmsg1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
