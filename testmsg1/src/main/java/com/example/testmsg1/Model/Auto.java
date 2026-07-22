92361bc1bbb77fb94856b62b2c46db1db550ed2b
patrickblg <blaga.patrick@yahoo.com>
2026-07-22 20:39:07 +0300

    Add auto model class

---
+++ testmsg1/src/main/java/com/example/testmsg1/Model/Auto.java
@@ -0,0 +1,16 @@
+package com.example.testmsg1.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Auto {
+    
+    private String name;
+
+    
+
+}
